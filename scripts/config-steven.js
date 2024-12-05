17ab3ea331c2627ae68c8dff7655469f63ee0b8c
Steven Hood <steven@range.com>
2024-12-05 10:33:34 -0800

    Ensure that focusing URL Bar uses cmd-D on Mac
    Not sure how this would play with Windows ... but that's ok, since
    it already does what I'm used to: alt+D

---
+++ scripts/config-steven.js
@@ -0,0 +1,43 @@
+// config1.js file for [Firefox program folder]
+// file name must match the name in [Firefox program folder]\defaults\pref
+function applyCustomScriptToNewWindow(win){
+  /*** Context menu changes ***/ 
+  var key = win.document.getElementById('focusURLBar');
+  key.setAttribute('key', 'D');
+  key.setAttribute('modifiers', 'accel');
+
+  var key = win.document.getElementById('addBookmarkAsKb');
+  key.setAttribute('key', 'B');
+  key.setAttribute('modifiers', 'accel');
+
+  /*** Other changes ***/ 
+}
+/* Single function userChrome.js loader to run the above init function (no external scripts)
+  derived from https://www.reddit.com/r/firefox/comments/kilmm2/ */
+
+try {
+  //let { classes: Cc, interfaces: Ci, manager: Cm  } = Components;
+  const Services = globalThis.Services;
+  function ConfigJS() { 
+    Services.obs.addObserver(this, 'chrome-document-global-created', false); 
+  }
+  ConfigJS.prototype = {
+    observe: function (aSubject) { 
+      aSubject.addEventListener('DOMContentLoaded', this, {once: true}); 
+    },
+    handleEvent: function (aEvent) {
+      let document = aEvent.originalTarget; 
+      let window = document.defaultView; 
+      let location = window.location;
+      if (/^(chrome:(?!\/\/(global\/content\/commonDialog|browser\/content\/webext-panels)\.x?html)|about:(?!blank))/i.test(location.href)) {
+        if (window._gBrowser) {
+          applyCustomScriptToNewWindow(window);
+        }
+      }
+    }
+  };
+  if (!Services.appinfo.inSafeMode) { 
+    new ConfigJS(); 
+  }
+} catch(ex) {};
+
